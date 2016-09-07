be1e87d0ab764253e314e366aed765de6a073c9e
Michael J. Simons <michael@simons.ac>
2016-09-07 10:40:24 +0200

    Create the ThymeleafBannerDialect bean
    Auto configuration is basically about creating beans, which we do through Java based configuration. This is not gonna run, though, as there isn't a BannerSupplier bean yet.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/wjax2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/wjax2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -3,6 +3,7 @@ package ac.simons.wjax2016.starter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
@@ -10,4 +11,9 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 @ConditionalOnClass({SpringTemplateEngine.class, ThymeleafAutoConfiguration.class})
 @AutoConfigureBefore(ThymeleafAutoConfiguration.class)
 class ThymeleafBannerAutoConfiguration {
+
+    @Bean
+    public ThymeleafBannerDialect webBannerDialect(final BannerSupplier bannerSupplier) {
+        return new ThymeleafBannerDialect(bannerSupplier);
+    }
 }
