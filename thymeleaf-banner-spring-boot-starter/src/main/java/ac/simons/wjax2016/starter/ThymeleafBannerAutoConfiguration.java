4469e9b536bca8cb4d645abd84769dd971d1ad76
Michael J. Simons <michael@simons.ac>
2016-09-07 11:10:33 +0200

    Instantiate the default BannerSupplier
    So now our demo starts… Lets add try the banner in the next step.

--- thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/wjax2016/starter/ThymeleafBannerAutoConfiguration.java
+++ thymeleaf-banner-spring-boot-starter/src/main/java/ac/simons/wjax2016/starter/ThymeleafBannerAutoConfiguration.java
@@ -1,16 +1,23 @@
 package ac.simons.wjax2016.starter;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
 @ConditionalOnClass({SpringTemplateEngine.class, ThymeleafAutoConfiguration.class})
 @AutoConfigureBefore(ThymeleafAutoConfiguration.class)
 class ThymeleafBannerAutoConfiguration {
+
+    @Bean
+    public BannerSupplier defaultBannerSupplier(final Environment environment, final Banner banner) {
+        return new DefaultBannerSupplier(environment, banner);
+    }
 
     @Bean
     public ThymeleafBannerDialect webBannerDialect(final BannerSupplier bannerSupplier) {
